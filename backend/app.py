537b1b79b5c7d7eb032a84560b7d005353ce18f3
behradrez <r.behrad121@gmail.com>
2024-03-31 23:30:30 -0400

    fix folder structure + bypassing reddit script detection

--- backend/app.py
+++ backend/app.py
@@ -5,16 +5,19 @@ import utils.analyzer as analyzer
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
     return 'Hello, World!'
 
 @app.route('/analyze/<ticker>')
 def analyze(ticker):
-    all_posts = scraper.get_all_discussions(ticker)
-    analysis = analyzer.get_overall_sentiment(all_posts)
-    percentage = analyzer.get_percentage_sentiment(analysis)
-    return f'Analyzing {ticker}: {analysis}. All texts: {all_posts} \n\n\n\nPercentages: {percentage[0]}% positive, {percentage[1]}% negative.'
-
+    try:
+        all_posts = scraper.get_all_discussions(ticker)
+        analysis = analyzer.get_overall_sentiment(all_posts)
+        percentage = analyzer.get_percentage_sentiment(analysis)
+        return f'Analyzing {ticker}: {analysis}. All texts: {all_posts} \n\n\n\nPercentages: {percentage[0]}% positive, {percentage[1]}% negative.'
+    except Exception as e:
+        return str(e)
+    
 @app.route("/analyze/percent/<ticker>")
 def analyze_percent(ticker):
     all_posts = scraper.get_all_discussions(ticker)
@@ -25,5 +28,6 @@ def analyze_percent(ticker):
                         "total":percentage[2]})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/utils/scraper.py
+++ backend/utils/scraper.py
@@ -8,23 +8,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
+#from webdriver_mananger.chrome import ChromeDriverManager
 
+def get_driver(headless=False):
+    options = Options()
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36")
+    #service = Service(ChromeDriverManager().install())
+    if headless:
+        options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    return driver
 
-def get_frontpage_post_URLs(ticker:str, WSB=True):
+def get_frontpage_post_URLs(ticker:str,driver, WSB=True):
     """Retrieves all URLs of posts on the front page of r/wallstreetbets that mention a given ticker
     ARGS: ticker (str): The ticker to search for
     RETURN: list: A list of URLs of posts that mention the given ticker"""
+    
     print("getting links")
     url = "https://www.reddit.com/r/wallstreetbets/search/?q=%2Bflair%3ADiscussion+"+ticker+"&type=link&cId=33c4f3c1-04bc-48c9-88ba-2e29358869c2&iId=dd948810-572a-476a-84e1-a666783866b0&sort=relevance&t=week"
     POST_URL_XPATH = "//a[@data-testid='post-title'][@href]"
+    
     if not WSB:
         url = "https://www.reddit.com/r/stocks/search/?q=%20"+ticker+"%20%20&restrict_sr=1&t=week"
-    driver = webdriver.Chrome()
-
+    
     
     driver.get(url)
-    #time.sleep(2)
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, POST_URL_XPATH)))
     all_posts = driver.find_elements(By.XPATH,POST_URL_XPATH)
+
     post_urls = []
     for post in all_posts:
         post_urls.append(post.get_attribute("href"))
@@ -33,7 +45,10 @@ def get_frontpage_post_URLs(ticker:str, WSB=True):
 #Old method for getting text from a single post
 def get_text_from_post(url:str):
     """Retrieves the text of a single post on reddit (OLD METHOD)"""
-    driver = webdriver.Chrome()
+    options = Options()
+    #options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    
     driver.get(url)
     post_text = ""
     
@@ -56,8 +71,7 @@ def get_text_from_post(url:str):
     #print(post_text)
     return post_text
 
-def get_texts_from_posts(urls:list):
-    driver = webdriver.Chrome()
+def get_texts_from_posts(urls:list, driver):
     
     #XPATHs for required elements on reddit post page
     TITLE_XPATH = "//h1[@slot='title']"
@@ -75,6 +89,7 @@ def get_texts_from_posts(urls:list):
             #Usually means reddit blocked the request because it was too fast, so just try again
             try:
                 driver.get(url)
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, TITLE_XPATH)))
             except:
                 #If it fails again, just skip this post
                 continue
@@ -107,10 +122,16 @@ def get_texts_from_posts(urls:list):
     return post_texts
 
 def get_all_discussions(ticker:str):
-    all_posts = get_frontpage_post_URLs(ticker)
-    all_texts = get_texts_from_posts(all_posts)
-    all_posts = get_frontpage_post_URLs(ticker, False)
-    all_texts += get_texts_from_posts(all_posts)
+    driver = get_driver(headless=True)
+
+    #Get all posts from r/wallstreetbets
+    all_posts = get_frontpage_post_URLs(ticker,driver)
+    all_texts = get_texts_from_posts(all_posts,driver)
+
+    #Get all posts from r/stocks
+    all_posts = get_frontpage_post_URLs(ticker,driver, False)
+    all_texts += get_texts_from_posts(all_posts,driver)
+
     return all_texts
     """for post in all_posts:
         text = get_text_from_post(post)
@@ -124,4 +145,5 @@ def get_all_discussions(ticker:str):
     return all_texts"""
 
 if __name__ == "__main__":
-    get_all_discussions("NVDA")
+    discussions = get_all_discussions("NVDA")
+    print(len(discussions))
